07e6cfde56b35e2accf209fbf3e90b65b974e002
xielianjun <xielianjun@meituan.com>
2021-08-05 13:20:25 +0800

    fix bug

--- dorado/test/src/main/java/com/meituan/dorado/client/Client.java
+++ dorado/test/src/main/java/com/meituan/dorado/client/Client.java
@@ -1,12 +1,16 @@
 package com.meituan.dorado.client;
 
 
+import com.meituan.dorado.rpc.AsyncContext;
+import com.meituan.dorado.rpc.ResponseFuture;
 import com.meituan.dorado.test.thrift.api.Echo;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.Callable;
 
 
 public class Client {
@@ -20,9 +24,17 @@ public class Client {
         LOGGER.info("mtthrift client started!");
 
         //***************************
-        //此处实现你的方法调用
+        //此处实现你的方法调用 同步
         String result = client.echo("Hello thrift");
-        LOGGER.info("result:{}", result);
+        LOGGER.info("*************result:{}", result);
+        // 异步
+        ResponseFuture<String> future = AsyncContext.getContext().asyncCall(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return client.echo("Hello thrift");
+            }
+        });
+        LOGGER.info("*************result:{}", result);
         //***************************
     }
 }
